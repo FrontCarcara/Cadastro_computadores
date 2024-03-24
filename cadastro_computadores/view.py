190305cd063034b1cdde52aa816ca10f8ff51511
FronTelli <143039167+FrontCarcara@users.noreply.github.com>
2024-03-23 22:32:53 -0300

    Update view.py

--- cadastro_computadores/view.py
+++ cadastro_computadores/view.py
@@ -36,11 +36,8 @@ def ver_unidades():
 def atulizar_unidades(i):
          with conexao:
              cursor = conexao.cursor()
-             query = "UPDATE unidades SET nome =?, unidade =? WHERE id=?"
+             query = "UPDATE unidades SET nome =?, sigla =? WHERE id=?"
              cursor.execute(query, i)
-l = ['BASE AEREA DE SAO PAULO','BASP','DOMBASP', 1]
-#atulizar_unidades(l)
-#print(ver_unidades())
 
 # Deletar as unidade (Delete D) CRUD
 def deletar_unidades(i):
@@ -49,3 +46,85 @@ def deletar_unidades(i):
         query = "DELETE FROM unidades WHERE id=?"
         cursor.execute(query, i)
 deletar_unidades([1])
+
+#COMANDOS DE SETOR CRUD--------------------------------------------------------------------------
+#Criar Unidades(Inserir C) CRUD
+def criar_setor(i):
+         with conexao:
+             cursor = conexao.cursor()
+             query = "INSERT INTO setores (nome, unidade, ramal) VALUES (?,?,?)"
+             cursor.execute(query, i)
+             
+
+#Ver todas as setores (Read R) CRUD
+def ver_setores():
+    lista = []
+    with conexao:
+        cursor = conexao.cursor()
+        cursor.execute('SELECT * FROM setores')
+        linha = cursor.fetchall()
+        
+        for i in linha:
+            lista.append(i)
+        return lista
+
+#Atualizar setores(Update U) CRUD
+def atulizar_setores(i):
+         with conexao:
+             cursor = conexao.cursor()
+             query = "UPDATE setores SET nome =?, unidade=?, ramal =? WHERE id=?"
+             cursor.execute(query, i)
+
+# Deletar as unidade (Delete D) CRUD
+def deletar_setores(i):
+    with conexao:
+        cursor = conexao.cursor()
+        query = "DELETE FROM setores WHERE id=?"
+        cursor.execute(query, i)
+deletar_setores([1])
+
+#COMANDOS DE COMPUTADORES CRUD--------------------------------------------------------------------------
+#Criar Unidades(Inserir C) CRUD
+def criar_computadores(i):
+         with conexao:
+             cursor = conexao.cursor()
+             query = "INSERT INTO computadores (setor, bmp, ramal, data_solicitado, data_retirada, solicitador, retirado, problema, telefone, pronto) VALUES (?,?,?,?,?,?,?,?,?,?)"
+             cursor.execute(query, i)
+             
+
+#Ver todas as computadores (Read R) CRUD
+def ver_computadores():
+    lista = []
+    with conexao:
+        cursor = conexao.cursor()
+        cursor.execute('SELECT * FROM computadores')
+        linha = cursor.fetchall()
+        
+        for i in linha:
+            lista.append(i)
+        return lista
+
+#Atualizar computadores(Update U) CRUD
+def atulizar_computadores(i):
+         with conexao:
+             cursor = conexao.cursor()
+             query = "UPDATE computadores SET setor =?, bmp=?, ramal =?, data_solicitado =?, data_retirada =?, solicitador=?, retirado=?, problema=?, telefone=?, pronto=? WHERE id=?"
+             cursor.execute(query, i)
+
+# Deletar as unidade (Delete D) CRUD
+def deletar_computadores(i):
+    with conexao:
+        cursor = conexao.cursor()
+        query = "DELETE FROM computadores WHERE id=?"
+        cursor.execute(query, i)
+deletar_computadores([1])
+
+#função procurar computador
+def procurar_computador(solicitador):
+    with conexao:
+        cursor = conexao.cursor()
+        cursor.execute('SELECT * FROM computadores WHERE solicitador =?', (solicitador,))
+        dados = cursor.fetchall()
+                
+        return dados
+
